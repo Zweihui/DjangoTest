78c22c20dec6bd6664de83350837add6bbc3e8e0
Zweihui <616505546@qq.com>
2018-01-11 17:08:06 +0800

    post excerpt

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
+import markdown
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 from django.urls import reverse
+from django.utils.html import strip_tags
 
 
 class Category(models.Model):
@@ -79,6 +81,19 @@ class Post(models.Model):
     def increase_views(self):
         self.views += 1
         self.save(update_fields=['views'])
+
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个 Markdown 类，用于渲染 body 的文本
+            md = markdown.Markdown(extensions=['extra', 'codehilite', 'toc'])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
 
     class Meta:
         ordering = ['-created_time']
