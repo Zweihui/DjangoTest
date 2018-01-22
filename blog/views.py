6042e7cf893a14a1b0a1fec71846752c6d4d4c35
Zweihui <616505546@qq.com>
2018-01-22 14:43:02 +0800

    2018_1_22_04

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.views.generic import ListView, DetailView
+from markdown.extensions.toc import TocExtension, slugify
 
 from blog.models import Post, Category, Tag
 from comments.forms import CommentForm
@@ -228,6 +229,14 @@ class PostDetailView(DetailView):
         # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
         post = super(PostDetailView, self).get_object(queryset=None)
         post.body = markdown.markdown(post.body, ['extra','codehilite','toc',])
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            # 记得在顶部引入 TocExtension 和 slugify
+            TocExtension(slugify=slugify),
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
         return post
 
     def get_context_data(self, **kwargs):
